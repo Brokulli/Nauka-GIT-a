b0a3b35b4874b34b38eea20a73898259096cff9c
Brokulli <respondek.patrycja@gmail.com>
2018-01-27 15:04:45 +0100

    aktualizacja

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+if __name__=="__main__":
+    imie = "Patrycja"
+    wiek = 26
+    print("{0}, {1}". format(...))
+
+    if wiek < 63:
+        print("Nie emeryt")
+    else:
+        print("Wiek produkcyjny")
+
+
+
 imie = "Patrycja"
-print("Hello world, {0}!". format(imie))
+nazwisko = "Res"
+
+pelne_imie = imie + " " + nazwisko
+
+ilosc_zierzat_domowych = 2
+cena = 2 * 50
+
+#print("Hello world, {0}!". format(imie))
